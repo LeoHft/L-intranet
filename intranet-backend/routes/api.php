b999663759952e33a807ea22b1dae259eadc100d
LeoHft <leo.hofstetter@orange.fr>
2025-07-07 21:44:10 +0200

    Fix

--- intranet-backend/routes/api.php
+++ intranet-backend/routes/api.php
@@ -8,9 +8,9 @@ use App\Http\Middleware\AdminMiddleware;
 
 
 // Login
-Route::post('intranet/login', [UserController::class, 'login']);
+Route::post('login', [UserController::class, 'login']);
 
-Route::get('intranet/test', function (Request $request) {
+Route::get('test', function (Request $request) {
     return response()->json(['message' => 'API is working']);
 });
 
@@ -19,16 +19,16 @@ Route::get('intranet/test', function (Request $request) {
 Route::middleware('jwt')->group(function () {
 
     //Logout
-    Route::post('intranet/logout', [UserController::class, 'logout']);
+    Route::post('logout', [UserController::class, 'logout']);
 
     //Services
-    Route::get('intranet/getUserServices', 'App\Http\Controllers\ServicesController@getUserServices'); // Récupère un service
+    Route::get('getUserServices', 'App\Http\Controllers\ServicesController@getUserServices'); // Récupère un service
 
     //Catégories
-    Route::get('intranet/getCategories', 'App\Http\Controllers\CategoryController@getCategory'); // Récupère les catégories
+    Route::get('getCategories', 'App\Http\Controllers\CategoryController@getCategory'); // Récupère les catégories
 
     //Status
-    Route::get('intranet/getStatus', 'App\Http\Controllers\StatusController@getStatus'); // Récupère les status
+    Route::get('getStatus', 'App\Http\Controllers\StatusController@getStatus'); // Récupère les status
 });
 
 
@@ -37,24 +37,24 @@ Route::middleware('jwt')->group(function () {
 Route::middleware(['jwt', 'admin'])->group(function() {
 
     //Services
-    Route::get('intranet/getServices', 'App\Http\Controllers\ServicesController@getServices'); // Récupère les services
-    Route::post('intranet/storeService', 'App\Http\Controllers\ServicesController@store'); // Enregistre un service 
-    Route::put('intranet/updateService/{id}', 'App\Http\Controllers\ServicesController@update'); // Modifie un service
-    Route::delete('intranet/deleteService/{id}', 'App\Http\Controllers\ServicesController@destroy'); // Supprime un service
+    Route::get('getServices', 'App\Http\Controllers\ServicesController@getServices'); // Récupère les services
+    Route::post('storeService', 'App\Http\Controllers\ServicesController@store'); // Enregistre un service 
+    Route::put('updateService/{id}', 'App\Http\Controllers\ServicesController@update'); // Modifie un service
+    Route::delete('deleteService/{id}', 'App\Http\Controllers\ServicesController@destroy'); // Supprime un service
 
     //Catégories
-    Route::post('intranet/storeCategory', 'App\Http\Controllers\CategoryController@store'); // Enregistre une catégorie
-    Route::put('intranet/updateCategory/{id}', 'App\Http\Controllers\CategoryController@update'); // Modifie une catégorie
-    Route::delete('intranet/deleteCategory/{id}', 'App\Http\Controllers\CategoryController@destroy'); // Supprime une catégorie
+    Route::post('storeCategory', 'App\Http\Controllers\CategoryController@store'); // Enregistre une catégorie
+    Route::put('updateCategory/{id}', 'App\Http\Controllers\CategoryController@update'); // Modifie une catégorie
+    Route::delete('deleteCategory/{id}', 'App\Http\Controllers\CategoryController@destroy'); // Supprime une catégorie
 
     //Status
-    Route::post('intranet/storeStatus', 'App\Http\Controllers\StatusController@store'); // Enregistre un status
-    Route::put('intranet/updateStatus/{id}', 'App\Http\Controllers\StatusController@update'); // Modifie un status
-    Route::delete('intranet/deleteStatus/{id}', 'App\Http\Controllers\StatusController@destroy'); // Supprime un status
+    Route::post('storeStatus', 'App\Http\Controllers\StatusController@store'); // Enregistre un status
+    Route::put('updateStatus/{id}', 'App\Http\Controllers\StatusController@update'); // Modifie un status
+    Route::delete('deleteStatus/{id}', 'App\Http\Controllers\StatusController@destroy'); // Supprime un status
 
     //utilisateurs
-    Route::get('intranet/getUsers', 'App\Http\Controllers\UserController@getUsers'); // Récupère les utilisateurs
-    Route::post('intranet/storeUser', 'App\Http\Controllers\UserController@store'); // Enregistre un utilisateur
-    Route::put('intranet/updateUser/{id}', 'App\Http\Controllers\UserController@update'); // Modifie un utilisateur
-    Route::delete('intranet/deleteUser/{id}', 'App\Http\Controllers\UserController@destroy'); // Supprime un utilisateur
+    Route::get('getUsers', 'App\Http\Controllers\UserController@getUsers'); // Récupère les utilisateurs
+    Route::post('storeUser', 'App\Http\Controllers\UserController@store'); // Enregistre un utilisateur
+    Route::put('updateUser/{id}', 'App\Http\Controllers\UserController@update'); // Modifie un utilisateur
+    Route::delete('deleteUser/{id}', 'App\Http\Controllers\UserController@destroy'); // Supprime un utilisateur
 });
